79ae7142e3892dc5ce7f7d354e3ff42af76f07c9
Garrett <garrett.odom@postlight.com>
2022-06-30 14:48:01 -0600

    move posts link to hamburger menu

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
       <header id="header">
         <h1><a href="/home">Be Cool! Be Gamer!</a></h1>
         <nav class="links">
-          <ul class="posts-link">
-            <li><a href="/home">Posts</a></li>
-          </ul>
         </nav>
         <nav class="main">
           <ul>
@@ -22,6 +19,7 @@
           <!-- Actions -->
           <section>
             <ul class="actions stacked">
+              <li><a class="button large fit" href="/home">All Posts</a></li>
               <li v-if="isLoggedIn()">
                 <button v-on:click="getUserID()" class="button large fit">My Account</button>
               </li>
@@ -168,10 +166,5 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
-
-.posts-link {
-  position: relative;
-  right: 95px;
 }
 </style>
